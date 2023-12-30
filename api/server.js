d3bee9c84819f6f5614398df6dc0d624c98c0450
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-30 15:18:01 +0545

    Custom error handler created

--- api/server.js
+++ api/server.js
@@ -18,3 +18,13 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server Error";
+  return res.status(statusCode).json({
+    sucess: false,
+    statusCode,
+    message,
+  });
+});
