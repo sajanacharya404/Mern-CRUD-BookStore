ab071dbbb6fb9f5a2b979d2ff4ac2f373a0eee5b
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-31 00:32:30 +0545

    login funcunality completed

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import { errorHandler } from "../utlis/error.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -17,7 +18,31 @@ export const registerUser = async (req, res, next) => {
     next(errorHandler(500, "Internal Server Error"));
   }
 };
-export const loginUser = async () => {};
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      res.status(404).json({ message: "User Not Found" });
+    }
+    const isPasswordValid = bcrypt.compareSync(password, validUser.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("token", token, { httpOnly: true, SameSite: "None" })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    console.error("Error:", error);
+    next(errorHandler(500, "Internal Server Error"));
+  }
+};
 export const getUser = async () => {};
 export const getUserById = async () => {};
 export const updateUser = async () => {};
