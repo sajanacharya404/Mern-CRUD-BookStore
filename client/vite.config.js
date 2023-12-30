9b5b500d2356accfbc43cd5313e482580ffcc8b4
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-30 17:33:17 +0545

    Proxy server created to handle api

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
