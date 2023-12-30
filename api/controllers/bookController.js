81752ce7ba4024afb69df276b5359796a79849d0
sikklelikestraight <sesmaniraj7@gmail.com>
2023-12-30 14:50:19 +0545

    getbooksbyid api created

--- api/controllers/bookController.js
+++ api/controllers/bookController.js
@@ -8,3 +8,8 @@ export const getBooks = async (req, res, next) => {
   const movies = await Books.find(req.body);
   res.status(200).json(movies);
 };
+export const getBooksById = async (req, res, next) => {
+  const { id } = req.params;
+  const movies = await Books.findById(id);
+  res.status(200).json(movies);
+};
--- api/routes/bookRoutes.js
+++ api/routes/bookRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getBooks, postBooks } from "../controllers/bookController.js";
+import {
+  getBooks,
+  getBooksById,
+  postBooks,
+} from "../controllers/bookController.js";
 
 const router = express.Router();
 
 router.post("/books", postBooks);
 router.get("/books", getBooks);
+router.get("/books/:id", getBooksById);
 
 export default router;
