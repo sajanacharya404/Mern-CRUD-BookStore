9ca10a2e8e6ff18f52a64cfcf67d152c30ecfc48
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-02 15:17:03 +0545

    Home page created with taliwind css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,43 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { FaBook } from "react-icons/fa";
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate("/books");
-  };
-  const handleViewClick = () => {
-    navigate("/viewbooks");
-  };
-  const handleLogin = () => {
-    navigate("/login");
-  };
-
   return (
-    <div className="flex items-center justify-center h-screen">
-      <button
-        onClick={handleLogin}
-        className="bg-blue-500 text-white py-2 px-4 rounded flex items-center space-x-2 mx-10"
-      >
-        <FaBook />
-        <span>Login</span>
-      </button>
-      <button
-        onClick={handleClick}
-        className="bg-blue-500 text-white py-2 px-4 rounded flex items-center space-x-2 mx-10"
-      >
-        <FaBook />
-        <span>Add Books</span>
-      </button>
-      <button
-        onClick={handleViewClick}
-        className="bg-blue-500 text-white py-2 px-4 rounded flex items-center space-x-2"
-      >
-        <FaBook />
-        <span>View Books</span>
-      </button>
+    <div className=" text-slate-900 h-89 flex items-center justify-center">
+      <div className="text-center p-8 max-w-lg overflow-y-auto">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Book Store</h1>
+        <p className="text-lg">
+          Discover a world of books and reading pleasure.
+        </p>
+      </div>
     </div>
   );
 };
