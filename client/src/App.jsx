c6e719ecfd5200c906e29a4fcb87913dc18e6efa
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-02 15:13:57 +0545

    Navbar created with taliwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from "./components/Home";
 import BookList from "./components/BookList";
 import UpdateBook from "./components/UpdateBook";
 import LoginPage from "./components/user/LoginPage";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FaBook } from "react-icons/fa";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/books");
+  };
+
+  const handleViewClick = () => {
+    navigate("/viewbooks");
+  };
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
+  return (
+    <div className="bg-blue-400 p-4 flex items-center justify-between">
+      <h1 className="text-white text-lg">
+        Book <span className="font-bold">Store</span>
+      </h1>
+      <div className="flex items-center space-x-4">
+        <button
+          onClick={handleClick}
+          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2"
+        >
+          <FaBook className="text-blue-400" />
+          <span>Add Books</span>
+        </button>
+        <button
+          onClick={handleViewClick}
+          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
+        >
+          <FaBook className="text-blue-400" />
+          <span>View Books</span>
+        </button>
+        <button
+          onClick={handleRegister}
+          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
+        >
+          <FaBook className="text-blue-400" />
+          <span>Register</span>
+        </button>
+        <button
+          onClick={handleLogin}
+          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
+        >
+          <FaBook className="text-blue-400" />
+          <span>Login</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
