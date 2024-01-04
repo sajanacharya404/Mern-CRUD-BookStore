d9eddb8ead289ced1f49a61d097919bf55bcfec4
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-04 21:24:39 +0545

    conditional added to show logout login and register

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,8 +23,11 @@ const Navbar = () => {
     await fetch("/api/logout", {
       method: "POST",
     });
+    localStorage.removeItem("token");
     navigate("/login");
   };
+
+  const isUser = localStorage.getItem("token");
 
   return (
     <div className="bg-blue-400 p-4 flex items-center justify-between">
@@ -32,41 +35,48 @@ const Navbar = () => {
         Book <span className="font-bold">Store</span>
       </h1>
       <div className="flex items-center space-x-4">
-        <button
-          onClick={handleClick}
-          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2"
-        >
-          <FaBook className="text-blue-400" />
-          <span>Add Books</span>
-        </button>
-        <button
-          onClick={handleViewClick}
-          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
-        >
-          <FaBook className="text-blue-400" />
-          <span>View Books</span>
-        </button>
-        <button
-          onClick={handleRegister}
-          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
-        >
-          <FaBook className="text-blue-400" />
-          <span>Register</span>
-        </button>
-        <button
-          onClick={handleLogin}
-          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
-        >
-          <FaBook className="text-blue-400" />
-          <span>Login</span>
-        </button>
-        <button
-          onClick={handleLogout}
-          className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
-        >
-          <FaBook className="text-blue-400" />
-          <span>Logout</span>
-        </button>
+        {isUser ? (
+          <>
+            <button
+              onClick={handleClick}
+              className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2"
+            >
+              <FaBook className="text-blue-400" />
+              <span>Add Books</span>
+            </button>
+            <button
+              onClick={handleViewClick}
+              className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
+            >
+              <FaBook className="text-blue-400" />
+              <span>View Books</span>
+            </button>
+            <button
+              onClick={handleLogout}
+              className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
+            >
+              <FaBook className="text-blue-400" />
+              <span>Logout</span>
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={handleRegister}
+              className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
+            >
+              <FaBook className="text-blue-400" />
+              <span>Register</span>
+            </button>
+            <button
+              onClick={handleLogin}
+              className="bg-white text-blue-400 p-1 rounded flex items-center space-x-2 border"
+            >
+              <FaBook className="text-blue-400" />
+              <span>Login</span>
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
